2efaff3dcd66c452e7cbaf725581c3d0a66f15b7
AlexandrIvanov <a.ivanov@revansh.info>
2023-01-27 12:50:39 +0400

    new file:   __debug_bin.exe 	new file:   main.go 	new file:   service/126.txt 	new file:   service/138.txt 	new file:   service/139.txt 	new file:   service/140.txt 	new file:   service/141.txt 	new file:   service/142.txt 	new file:   service/143.txt 	new file:   service/144.txt 	new file:   service/17.txt 	new file:   service/18.txt 	new file:   service/19.txt 	new file:   service/20.txt 	new file:   service/21.txt 	new file:   service/22.txt 	new file:   service/23.txt 	new file:   service/3.txt 	new file:   service/32.txt 	new file:   service/33.txt 	new file:   service/37.txt 	new file:   service/4.txt 	new file:   service/44.txt 	new file:   service/49.txt 	new file:   service/5.txt 	new file:   service/6.txt 	new file:   service/64.txt 	new file:   service/68.txt 	new file:   service/7.txt 	new file:   service/72.txt 	new file:   service/77.txt 	new file:   service/80.txt 	new file:   service/81.txt 	new file:   service/89.txt 	new file:   service/9.txt 	new file:   service/91.txt 	new file:   service/92.txt

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+ 
+import (
+	//"fmt"
+	"log"
+	"net/http"
+	//"strconv" 
+	"strings" // сплитим адрес для айдишников 
+	"os"
+)
+
+
+
+// type Docs struct {
+
+// 	Doc_Id int 
+
+// 	Text string 
+// }
+
+
+
+
+
+ 
+func home(w http.ResponseWriter, r *http.Request) {
+	// Проверяется, если текущий путь URL запроса точно совпадает с шаблоном "/". Если нет, вызывается
+	// функция http.NotFound() для возвращения клиенту ошибки 404.
+	// Важно, чтобы мы завершили работу обработчика через return. Если мы забудем про "return", то обработчик
+	// продолжит работу и выведет сообщение  как ни в чем не бывало.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+ 
+	w.Write([]byte("Просто Отсканируйте Код"))
+}
+ 
+// Обработчик для отображения содержимого заметки.
+func showDescription(w http.ResponseWriter, r *http.Request) {
+	// Извлекаем значение параметра id из URL и попытаемся
+	// конвертировать строку в integer используя функцию strconv.Atoi(). Если его нельзя
+	// конвертировать в integer, или значение меньше 1, возвращаем ответ
+
+	id := r.URL.Query().Get("id") // получаем строку с айдишками
+
+	//fmt.Println(id)
+
+	strsplit := strings.Split(id,",") // разделяем по запятой 
+
+	//fmt.Println(strsplit)
+
+
+	// проходимся циклом по массиву, чтобы выцепить айди 
+	for docnumber := range strsplit {
+  	 	//fmt.Println(strsplit[docnumber])
+
+  	 	filename :=string("service/"+strsplit[docnumber]+".txt")
+
+  	 	file, err := os.Open(filename)
+    	if err != nil {
+        	log.Fatal(err)
+        	//w.Write([]byte(""))
+   		 }
+    	defer func() {
+        	if err = file.Close(); err != nil {
+            	log.Fatal(err)
+       		}
+   		}()
+ 		b, err := os.ReadFile(file)
+  		w.Write([]byte(b))
+	 }
+
+
+
+	
+
+	 // if err != nil || id < 1 
+	 // {
+	 // 	http.NotFound(w, r)
+	 // 	return
+	 // }
+ 	
+	
+	// Используем функцию fmt.Fprintf() для вставки значения из id в строку ответа
+	// и записываем его в http.ResponseWriter.
+	//fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...",id)
+}
+ 
+
+ 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/description", showDescription)
+	
+	log.Println("Запуск веб-сервера на http://127.0.0.1:8080(locallhost)")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
